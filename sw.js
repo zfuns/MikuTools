f5c218665f52ac9bd9cffcb6aac7be6f6917f3c8
Ice-Hazymoon <imiku.me@gmail.com>
2019-03-22 12:53:50 +0000

    automated build

---
+++ sw.js
@@ -0,0 +1,314 @@
+importScripts('https://mikutools.cdn.hazymoon.vip/workbox.4c4f5ca6.js')
+
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/0177c1638a25d07a87c2.js",
+    "revision": "b52e19a944c0e4a35f26addbaa89fa4f"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/04715885b604df8cb6fb.js",
+    "revision": "5ac618b95c96943debf6b8a61f70b44d"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/04ea6e13e072cb369568.js",
+    "revision": "6564c839022bcb83ac93c8e0bfd86f10"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/090a045b340439bde107.js",
+    "revision": "12c87f3204fbaa1957f3a19d9778eb8d"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/11e9b7e996a1e7395638.js",
+    "revision": "789936e3aa43b769f4bb7d711bc0abde"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/161a18b84e1181aba9b4.js",
+    "revision": "9bf2317da0718b50ea690bbc302d9bb6"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/1d2fa7c90294ef10dbf9.js",
+    "revision": "6f507cde3ab224cef41f3131458b5e52"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/1f5898b1d6fdc7362a99.js",
+    "revision": "0817886f0b1e81ec810689f2998e4c83"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/209e38f53fa02a37aa2e.js",
+    "revision": "d59ea92af7e154b5d68107419b32261b"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/2581b9263fe6ce565585.js",
+    "revision": "4c267e3733b10d2c52fb886d2ccbca78"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/296b2be33fde1d1f05a8.js",
+    "revision": "47496766947b0d5c2bbe7fef6c15da9f"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/2e4704744ce4a454f4dc.js",
+    "revision": "587597ffa7e67bbdb7f64f17bb1ad4be"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/32305e5cbf06938ab922.js",
+    "revision": "ecc9f1354993b797fbcb84a2986dcb4f"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/36ede48954295765ebd0.js",
+    "revision": "272b2b33720823bac817cb7fc5bc75a0"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/379b770effca535c0a01.js",
+    "revision": "645d2a1400223203b99f42998d7f8f91"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/3826a4a6d023a79e0455.js",
+    "revision": "16b70a95130f88531e67e14a2f0fa0dd"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/384991db5bf13c608a95.js",
+    "revision": "ab009257c0184d570464d8969caeb1d6"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/3853da64018fcb0cb26b.js",
+    "revision": "3686d9e46e48461446a0916e797df79d"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/3db82c423559621665f8.js",
+    "revision": "02ca8c4cae169a877a94ccf12702b546"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/433aa861517a10416293.js",
+    "revision": "f70c8d06e8ccb568dcc2727163335e3b"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/45e0c13c7097f26ac3f5.js",
+    "revision": "3b1f7146f160713529f05a59409477cf"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/46e199008053b38f7980.js",
+    "revision": "a2771fb83bddf7e77082e2a151bc0c0b"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/46f939d453953d6fb436.js",
+    "revision": "70a14ce101cfab2862e2fd4fc9b8bb98"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/511fdce247bcfe4ee74c.js",
+    "revision": "736b6ab0abe692ff4adeddf964dec3b9"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/536f29281140096c5fa9.js",
+    "revision": "c339bed7113205948eb7a69232f40637"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/539e5d40168d077c26b4.js",
+    "revision": "1ec568a3140ea23dba809201a62bd806"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/54c0c255ef507b72697c.js",
+    "revision": "df3079f20d077abcabd923532f193c4e"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/5682da4af760e23e60f8.js",
+    "revision": "f299b853e5437c55b8a26c9d4c9aae28"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/5690e4b5c419f1f0deb6.js",
+    "revision": "556ff25127db8ee35c3c6ea399bd503a"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/57fceb088514a41d7305.js",
+    "revision": "b2145f5d81b1eeaa3d7e1a5244be8780"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/5c657f780782cc5b30dc.js",
+    "revision": "48995cb21d2ddd9a064a9321fab8425e"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/5d051c65e99b6fa34492.js",
+    "revision": "24447a89ea2df437a5993f39045c33ee"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/5df3db81fd6683082a98.js",
+    "revision": "8ae1cf69c4256402e8f894022cc977b4"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/5f661ee837ca7a187feb.js",
+    "revision": "6381813f47e796faaa6405b25c414347"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/61b2e86d1866cc37cde2.js",
+    "revision": "17e6b22fa99f7dc7a664aab4a8d08a74"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/6987a6fee26b8454225f.js",
+    "revision": "4b2f9b9a0330a6988556e571d65591b0"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/6b8f65093367628193a2.js",
+    "revision": "0825cbac04acf4455157db9fd7ef0722"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/72fd2e0b6cb25a0e1cf6.js",
+    "revision": "9f002c2919c27c2dad7e6b42e22cf364"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/74af02aec00cb45a45d8.js",
+    "revision": "736cd1370c1ab01776c655f6550dd8a8"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/7861919909695470c553.js",
+    "revision": "3787840cc87ec7027741cb828a1fd957"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/79435b0ca93c6f6f74ad.js",
+    "revision": "59ee588eedb99fd622b3268286f35286"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/7a6e14a06e18dd0e6a70.js",
+    "revision": "b4ab95503067b577f3ec64d4034bad53"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/7ef10dd70eec43347aea.js",
+    "revision": "9e5ae2e5a01928791725a515bbd67a74"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/819522ebd2263d7676b9.js",
+    "revision": "eaa1facb4200f961411cbc8d7a874d04"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/8773403c3d109f287921.js",
+    "revision": "83ab22ea915b82f0f46ae50906059e48"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/8e24544c97214aa3f81e.js",
+    "revision": "b339016e7b128cb8e5bc1f4eb110559e"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/8f5a92c3ca9f6bdcfb2e.js",
+    "revision": "440a50fb8e9ff0e3a2f9c4430bcd4ec8"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/947d05b5efc3073eacb3.js",
+    "revision": "4b1f262de9f7057d56d646d64f1865ad"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/9887180699774a6b9d5c.js",
+    "revision": "78cecc4c3f13f69a642ae2a5a149a0a5"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/996f297b14643143817d.js",
+    "revision": "0424f32d54c0a59badeaa05eff52fd41"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/9ec58acd1c6df67bc7af.js",
+    "revision": "9616b24b20e4df8372497e2522a81000"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/a1387337fc3bfd5ce010.js",
+    "revision": "bc6e68f18658a6af0f71005f88099948"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/aa2485131eb2b3f64918.js",
+    "revision": "5439d3f73b5d022b7f492406609cb591"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/af9cd76b108e30864f76.js",
+    "revision": "1efda157c81dd7b82294fffd379607f2"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/b379a470c3f9c4db1cef.js",
+    "revision": "6251fdc265b05b781e219b5792c3c5df"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/b8ef5cfc5dd74938f70b.js",
+    "revision": "266d1b26b99e1ec593ebd328ce973720"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/b9f1698831d646bc0977.js",
+    "revision": "913917226f60a3356e55adf7d24df569"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/b9f9b9a7502b6138c966.js",
+    "revision": "71574cf4f8bbcbfe5ebef8981368f8d0"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/bfb5dd33076926d10ea9.js",
+    "revision": "f6ab4d510386b0a3ea37d9d4013bbf29"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/c02a3a426272eb8dde5b.js",
+    "revision": "9ddeaef420f67171b001ae061aea2d37"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/c568479a2982261ba6de.js",
+    "revision": "64ffebdf7dcfaceec9fa2c9e7534da56"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/cf126e9bada665eb3aae.js",
+    "revision": "3e42a86682d2d0d5a17698d987c47b57"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/d8658aa9ffe39afdfe87.js",
+    "revision": "2b970acc2ebbc478f4753f25ef40c2a2"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/db4e6924135d3be1cdd3.js",
+    "revision": "5025ab6e89210a5b6be9b88c2ec22e2e"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/db946755ebd20482fd70.js",
+    "revision": "56ca9fad29d81228ff8abb6431ff0d33"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/dc22e3e1ea7f4bb1506c.js",
+    "revision": "013ecf2bec776ab7fed9728e09121c43"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/e06dee4363564254f9f7.js",
+    "revision": "a20b4b21bfa9ca49fc4d2d2f04a5c5bb"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/e11145a021ff8127f42c.js",
+    "revision": "0a584a01259611ef618d5ea208261967"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/efa931917119c576438c.js",
+    "revision": "227088508400a0c5b35b8be7ea5cb81b"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/f3ac2bb3f6d28779791f.js",
+    "revision": "fbddb55553fb69b08e36a592bffeb2b2"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/f74401f2877ac09690d3.js",
+    "revision": "433995d2d669c21a691897f58a0f7e07"
+  },
+  {
+    "url": "https://mikutools.cdn.hazymoon.vip/f767f4cdd5cbfb6f740a.js",
+    "revision": "2989e2c7cf55824de26214df49a935b1"
+  }
+], {
+  "cacheId": "miku-tools",
+  "directoryIndex": "/",
+  "cleanUrls": false
+})
+
+workbox.precaching.precacheAndRoute(['/offline.html'])
+
+workbox.precaching.precacheAndRoute(['/offline.html'])
+
+workbox.clientsClaim()
+workbox.skipWaiting()
+
+workbox.routing.registerRoute(new RegExp('https://mikutools.cdn.hazymoon.vip/.*'), workbox.strategies.cacheFirst({}), 'GET')
+
+workbox.routing.registerRoute(new RegExp('https://mikutools.cdn.hazymoon.vip/.*'), workbox.strategies.networkFirst({}), 'GET')
+
+// offlinePage support
+const strategy = workbox.strategies.networkOnly()
+workbox.routing.registerRoute(new RegExp('/.*'), ({event}) => {
+  return strategy.handle({event})
+    .catch(() => caches.match('/offline.html'))
+})
